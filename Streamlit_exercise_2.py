7cf673b59c8d2ba16aea7be6c4a91d42e71b4e90
Codeharbor-01 <aadritthapa01@gmail.com>
2026-09-01 17:42:26 +0545

    Updated Electricity Price Calculator

--- Streamlit_exercise_2.py
+++ Streamlit_exercise_2.py
@@ -1,3 +1,35 @@
 import streamlit as st
 
 st.title('Electricity Price Calculator')
+
+col1,col2 = st.columns(2)
+
+fN = col1.text_input('First Name',placeholder='John')
+lN = col2.text_input('Last Name',placeholder='Doe')
+
+elec_consumption = st.number_input('Electricity Consumption in KWh',min_value=0)
+
+button = st.button(label = 'Calculate')
+
+if button:
+    if not (fN and lN and elec_consumption):
+        st.error('All fields are mandatory!')
+    else:
+        if 0<elec_consumption<=20:
+            ppu = 4
+        elif 21<=elec_consumption<=30:
+            ppu = 6.5
+        elif 30<=elec_consumption<=50:
+            ppu= 10
+        elif elec_consumption>50:
+            ppu = 12
+
+        total_price = elec_consumption*ppu
+
+        st.header('Calculated Fields')
+        df = st.dataframe({
+            'First Name':fN,
+            'Last Name':lN,
+            'Total Electricity consumption':elec_consumption,
+            'Total Amount':[total_price]
+        })
